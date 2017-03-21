81296c25919e6a6ad9b3f0f33f422f9f31dc1dae
Joshua Vial <joshua@enspiral.com>
2017-03-21 21:21:35 +1300

    simple hello file

--- lib/musing_cobra.ex
+++ lib/musing_cobra.ex
@@ -1,2 +1,5 @@
 defmodule MusingCobra do
+  def hello do
+    IO.puts "hello"
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule MusingCobra.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :extwitter]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,8 @@ defmodule MusingCobra.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+     {:extwitter, "~> 0.8"}
+    ]
   end
 end
